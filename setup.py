d78b6f16a057f9c4ed680ebf036c80314a9e4aae
dunamix <techwithdunamix2gmai.com>
2024-11-23 04:43:48 +0100

    feat : bilding the command line tool

--- setup.py
+++ setup.py
@@ -19,4 +19,9 @@ setup(
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.8",
+    entry_points={
+        'console_scripts': [
+            'nexio=nexio.cli.main:main',
+        ],
+    },
 )
